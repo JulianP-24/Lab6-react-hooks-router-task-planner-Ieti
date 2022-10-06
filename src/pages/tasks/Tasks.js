42c4e62536c105c1e051e113dc0791bb23d98b88
Julian Peña <julian.pena-m@mail.escuelaing.edu.co>
2022-10-05 20:04:09 -0500

    Cambiando la tabla por un dataGrid de Material UI

--- src/pages/tasks/Tasks.js
+++ src/pages/tasks/Tasks.js
@@ -1,56 +1,61 @@
 import React, { useState, useEffect } from "react";
 import tasksService from "../../services/tasksService";
+import { DataGrid } from "@mui/x-data-grid";
 
 function Tasks() {
+  const columnas = [
+    {
+      title: "Id",
+      field: "id",
+    },
+    {
+      title: "Name",
+      field: "name",
+    },
+    {
+      title: "Descripcion",
+      field: "description",
+    },
+    {
+      title: "Status",
+      field: "status",
+    },
+    {
+      title: "AssignedTo",
+      field: "assignedTo",
+    },
+    {
+      title: "DueDate",
+      field: "dueDate",
+    },
+    {
+      title: "CreatedAt",
+      field: "createdAt",
+    },
+  ];
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
     tasksService.getTasks().then((response) => {
-      console.log(response)
-        setTasks(response);
-    })
-        
+      console.log(response);
+      setTasks(response);
+    });
   }, []);
 
-  function listProducts() {
-    if (tasks) {
-      console.log(tasks);
-      return tasks.map((task) => {
-        return (
-          <tr key={task.id}>
-            <td>{task.name}</td>
-            <td>{task.descripcion}</td>
-            <td>{task.status}</td>
-            <td>{task.assignedTo}</td>
-            <td>{task.dueDate}</td>
-            <td>{task.createdAt}</td>
-          </tr>
-        );
-      });
-    }
-  }
-
   return (
-    <div>
-      <h1>Tasks: </h1>
-      <div>
-        <div>
-          <table>
-            <thead>
-              <tr>
-                <th>Nombre</th>
-                <th>Descripcion</th>
-                <th>Estado</th>
-                <th>Asignado</th>
-                <th>Fecha_Terminacion</th>
-                <th>Fecha_Creacion</th>
-              </tr>
-            </thead>
-            <tbody>{listProducts()}</tbody>
-          </table>
-        </div>
+    <center>
+      <div style={{ height: 400, width: "70%", backgroundColor: "white" }}>
+        <h1>Tasks: </h1>
+        <DataGrid
+          rows={tasks}
+          columns={columnas}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+          checkboxSelection
+          disableSelectionOnClick
+        />
       </div>
-    </div>
+    </center>
   );
 }
 
